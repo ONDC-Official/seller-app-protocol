2aa0d8f3fd851c1fa6386edda64609d8d88baea7
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-08-18 15:32:40 +0530

    Optimise code and config

--- webserver/main/__init__.py
+++ webserver/main/__init__.py
@@ -1,17 +1,9 @@
-import os
-
 from flask import Flask
 from flask_cors import CORS
-from flask_executor import Executor
-from flask_s3 import FlaskS3
 
 import main.config
 from main.models import init_database
 from main.routes import Api, api
-
-s3 = FlaskS3()
-jwt = None
-executor: Executor = None
 
 
 def create_app(config_name):
--- webserver/main/config.py
+++ webserver/main/config.py
@@ -1,7 +1,4 @@
 import os
-
-# uncomment the line below for postgres database url from environment variable
-# postgres_local_base = os.environ['DATABASE_URL']
 from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -34,57 +31,24 @@ class Config:
     BECKN_SECURITY_ENABLED = False
     BAP_PRIVATE_KEY = "some-key"
     BAP_KEY_ID = "default-key"
+    RABBITMQ_QUEUE_NAME = "bpp_protocol"
+    BPP_CLIENT_ENDPOINT = "http://client-endpoint"
 
 
 class DevelopmentConfig(Config):
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     ENV = True
-    # SQLALCHEMY_DATABASE_URI = "postgresql://flask:flask@localhost:5433/flask"
-    BAP_URL = "http://localhost:9002/protocol/v1"
-    MONGO_DATABASE_HOST = "localhost"
-    MONGO_DATABASE_PORT = 27017
-    MONGO_DATABASE_NAME = "sandbox_bap"
-
-
-class TestingConfig(Config):
-    DEBUG = True
-    PORT = 9901
-    SQLALCHEMY_DATABASE_URI = "postgresql://flask:flask@localhost:5433/flask"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DOMAIN = "http://localhost:9900"
-    PROPOGATE_EXCEPTIONS = True
-    REDSHIFT_ANALYTICS_SCHEMA = "analytics"
+    RABBITMQ_HOST = "localhost"
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
-    JWT_COOKIE_CSRF_PROTECT = False
-    MMI_CLIENT_ID = os.getenv("MMI_CLIENT_ID")
-    MMI_CLIENT_SECRET = os.getenv("MMI_CLIENT_SECRET")
-    MMI_ADVANCE_API_KEY = os.getenv("MMI_ADVANCE_API_KEY")
-    BAP_URL = os.getenv("BAP_URL", "http://localhost:9002/protocol/v1")
-    MONGO_DATABASE_HOST = os.getenv("MONGO_DATABASE_HOST", "mongo")
-    MONGO_DATABASE_PORT = int(os.getenv("MONGO_DATABASE_PORT", 27017))
-    MONGO_DATABASE_NAME = os.getenv("MONGO_DATABASE_NAME", "sandbox_bap")
-
-
-class LightConfig(Config):
-    DEBUG = False
-    # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
-    JWT_COOKIE_CSRF_PROTECT = False
-    ZENDRIVE_API_KEY = os.getenv("ZENDRIVE_API_KEY")
+    RABBITMQ_HOST = "rabbitmq"
 
 
 config_by_name = dict(
     dev=DevelopmentConfig,
-    test=TestingConfig,
     prod=ProductionConfig,
-    light=LightConfig,
 )
 
 key = Config.SECRET_KEY
@@ -102,11 +66,3 @@ def get_email_config_value_for_name(config_name):
     email_config_value = get_config_by_name("SES") or {}
     config = email_config_value.get(config_name)
     return config
-
-
-if __name__ == '__main__':
-    os.environ["ENV"] = "light"
-    print(get_config_by_name("DOMAIN"))
-
-    os.environ["ENV"] = "prod"
-    print(get_email_config_value_for_name("from_email"))
