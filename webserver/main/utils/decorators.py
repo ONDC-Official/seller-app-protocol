5562d41e1fd13231dcd95707397d7457deca7f2f
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-09-19 17:16:01 +0530

    Remove unused decorators

--- webserver/main/utils/decorators.py
+++ webserver/main/utils/decorators.py
@@ -1,14 +1,14 @@
 import traceback
 
-from flask_expects_json import expects_json
-from jsonschema.exceptions import ValidationError
-
-
-def expects_json_handling_validation(*args, **kwargs):
-    try:
-        return expects_json(*args, **kwargs)
-    except:
-        print("comig here")
+# from flask_expects_json import expects_json
+# from jsonschema.exceptions import ValidationError
+#
+#
+# def expects_json_handling_validation(*args, **kwargs):
+#     try:
+#         return expects_json(*args, **kwargs)
+#     except:
+#         print("comig here")
 
 
 def check_for_exception(func):
