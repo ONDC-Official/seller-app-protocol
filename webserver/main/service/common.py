cd385581921ca409dc9fd6f5e03d87e773c2cc2a
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-12-06 12:08:53 +0530

    Fix common service function for handling domain prefix

--- webserver/main/service/common.py
+++ webserver/main/service/common.py
@@ -14,13 +14,13 @@ from main.utils.lookup_utils import fetch_gateway_url_from_lookup
 
 @check_for_exception
 def send_bpp_responses_to_bg_or_bpp(message):
-    request_type = message['request_type']
-    log(f"{request_type} payload: {message}")
-    message_id = message['message_ids'][request_type]
-    payload = get_first_ondc_request(OndcDomain.RETAIL, OndcAction(request_type), message_id)
-    client_responses, _ = get_responses_from_client(request_type, payload)
+    log(f"{message['request_type']} payload: {message}")
+    action = message['request_type'].split("_")[-1]
+    message_id = message['message_ids'][action]
+    payload = get_first_ondc_request(OndcDomain.RETAIL, OndcAction(action), message_id)
+    client_responses, _ = get_responses_from_client(action, payload)
 
-    gateway_or_bap_endpoint = fetch_gateway_url_from_lookup() if request_type == "search" else \
+    gateway_or_bap_endpoint = fetch_gateway_url_from_lookup() if action == "search" else \
         payload['context']['bap_uri']
     status_code = make_request_over_ondc_network(client_responses, gateway_or_bap_endpoint,
                                                  client_responses['context']['action'])
