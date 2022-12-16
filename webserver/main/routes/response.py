337632371154f1dee446622576e82ab469baba96
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-12-16 10:42:26 +0530

    Update network-request-payload api args

--- webserver/main/routes/response.py
+++ webserver/main/routes/response.py
@@ -13,9 +13,13 @@ class GetNetworkRequestPayloads(Resource):
         parser.add_argument("select", dest='retail_select', required=False)
         parser.add_argument("init", dest="retail_init", required=False)
         parser.add_argument("confirm", dest="retail_confirm", required=False)
+        parser.add_argument("status", dest="retail_status", required=False)
+        parser.add_argument("track", dest="retail_track", required=False)
         parser.add_argument("logisticsOnSearch", dest="logistics_on_search", required=False)
         parser.add_argument("logisticsOnInit", dest="logistics_on_init", required=False)
         parser.add_argument("logisticsOnConfirm", dest="logistics_on_confirm", required=False)
+        parser.add_argument("logisticsOnStatus", dest="logistics_on_status", required=False)
+        parser.add_argument("logisticsOnTrack", dest="logistics_on_track", required=False)
         return parser.parse_args()
 
     def get(self):
