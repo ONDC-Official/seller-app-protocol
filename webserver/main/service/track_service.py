184a460dca754f761e4b857f7afffa3f1d52dad8
adityapatil123 <ad1tya.patil.3297@gmail.com>
2022-12-19 14:35:10 +0530

    Fix logistics bpp endpoint for track and status

--- webserver/main/service/track_service.py
+++ webserver/main/service/track_service.py
@@ -1,16 +1,15 @@
 from main.logger.custom_logging import log
 from main.models.ondc_request import OndcDomain, OndcAction
-from main.repository.db import get_first_ondc_request, get_ondc_requests
+from main.repository.db import get_first_ondc_request
 from main.service.common import get_responses_from_client
 from main.service.utils import make_request_over_ondc_network
 from main.utils.decorators import check_for_exception
-from main.utils.lookup_utils import fetch_gateway_url_from_lookup
 from main.utils.webhook_utils import post_on_bg_or_bap
 
 
 def make_logistics_track_request(payload):
-    gateway_or_bap_endpoint = fetch_gateway_url_from_lookup()
-    status_code = make_request_over_ondc_network(payload, gateway_or_bap_endpoint, payload['context']['action'])
+    bpp_endpoint = payload['context']['bpp_uri']
+    status_code = make_request_over_ondc_network(payload, bpp_endpoint, payload['context']['action'])
     log(f"Sent request to logistics-bg with status-code {status_code}")
 
 
