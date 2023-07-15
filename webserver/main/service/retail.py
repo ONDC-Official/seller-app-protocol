0aee4cdf4337ae88d703e5f8782d9bf47f374e53
Aditya Patil <ad1tya.patil.3297@gmail.com>
2023-07-15 11:23:40 +0530

    Update client routes

--- webserver/main/service/retail.py
+++ webserver/main/service/retail.py
@@ -10,9 +10,9 @@ from main.utils.lookup_utils import fetch_gateway_url_from_lookup
 
 def make_retail_payload_request_to_client(payload, request_type: OndcAction):
     if payload[constant.CONTEXT]["core_version"] != "1.2.0":
-        return get_responses_from_client(f"client/v1/{request_type.value}", payload)
+        return get_responses_from_client(f"v1/client/{request_type.value}", payload)
     else:
-        return get_responses_from_client(f"client/v2/{request_type.value}", payload)
+        return get_responses_from_client(f"v2/client/{request_type.value}", payload)
 
 
 @check_for_exception
