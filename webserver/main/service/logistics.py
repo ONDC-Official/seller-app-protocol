b875d567fde0e2c135bcd0fd8a3834b46b9425a4
Aditya Patil <ad1tya.patil.3297@gmail.com>
2023-10-20 15:01:14 +0530

    Update logistics search request

--- webserver/main/service/logistics.py
+++ webserver/main/service/logistics.py
@@ -1,5 +1,7 @@
 from main.logger.custom_logging import log
+from main.models import get_mongo_collection
 from main.models.ondc_request import OndcDomain, OndcAction
+from main.repository import mongo
 from main.repository.db import get_first_ondc_request
 from main.service.common import get_responses_from_client
 from main.service.utils import make_request_over_ondc_network
@@ -16,10 +18,11 @@ def make_logistics_request_payload_request_to_client(payload, retail_type: OndcA
 def make_logistics_request(message, request_type: OndcAction):
     log(f"logistics payload: {message}")
     message_id = message['message_ids'][request_type.value]
-    payload = get_first_ondc_request(OndcDomain.LOGISTICS, request_type, message_id)
-    payload['context']['bap_uri'] = f"{payload['context']['bap_uri']}/protocol/logistics/v1"
-    gateway_or_bap_endpoint = fetch_gateway_url_from_lookup()
-    status_code = make_request_over_ondc_network(payload, gateway_or_bap_endpoint, payload['context']['action'])
+    collection = get_mongo_collection(request_type.value)
+    query_object = {"context.message_id": message_id}
+    request_payload = mongo.collection_find_one(collection, query_object)
+    bap_endpoint = fetch_gateway_url_from_lookup(domain=request_payload['context']['action'])
+    status_code = make_request_over_ondc_network(request_payload, bap_endpoint, request_type.value)
     log(f"Sent request to logistics-bg with status-code {status_code}")
 
 
