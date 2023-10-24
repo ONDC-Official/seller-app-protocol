31f606177639683f160ccbc86e9500eb04c0b47f
Aditya Patil <ad1tya.patil.3297@gmail.com>
2023-10-25 00:06:44 +0530

    Update network endpoint for ondc-requests

--- webserver/main/service/logistics.py
+++ webserver/main/service/logistics.py
@@ -21,8 +21,11 @@ def make_logistics_request(message, request_type: OndcAction):
     collection = get_mongo_collection(request_type.value)
     query_object = {"context.message_id": message_id}
     request_payload = mongo.collection_find_one(collection, query_object)
-    bap_endpoint = fetch_gateway_url_from_lookup(domain=request_payload['context']['action'])
-    status_code = make_request_over_ondc_network(request_payload, bap_endpoint, request_type.value)
+    if request_payload['context']['action'] == "search":
+        endpoint = fetch_gateway_url_from_lookup(domain=request_payload['context']['domain'])
+    else:
+        endpoint = request_payload['context']['bpp_uri']
+    status_code = make_request_over_ondc_network(request_payload, endpoint, request_type.value)
     log(f"Sent request to logistics-bg with status-code {status_code}")
 
 
