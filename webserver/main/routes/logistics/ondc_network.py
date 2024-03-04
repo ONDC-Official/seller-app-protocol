da9feb495a084fe769c3a8e3bf495dcd0579a4ca
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-03-04 19:57:51 +0530

    Send logistics seller callbacks to client

--- webserver/main/routes/logistics/ondc_network.py
+++ webserver/main/routes/logistics/ondc_network.py
@@ -4,13 +4,13 @@ from flask_restx import Namespace, Resource
 from main import constant
 from main.models.ondc_request import OndcDomain, OndcAction
 from main.service.common import dump_request_payload
+from main.service.logistics import send_logistics_payload_to_client
 from main.utils.decorators import validate_auth_header
 from main.utils.validation import validate_payload_schema_based_on_version
 from main.service.retail import send_retail_payload_to_client_logistics
 from main.logger.custom_logging import log
 
-logistics_ondc_network_namespace = Namespace(
-    "logistics_ondc_network", description="Logistics ONDC Network Namespace")
+logistics_ondc_network_namespace = Namespace("logistics_ondc_network", description="Logistics ONDC Network Namespace")
 
 
 @logistics_ondc_network_namespace.route("/v1/on_search")
@@ -19,11 +19,13 @@ class OnSearchRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_search", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_search", domain="logistics")
         if resp is None:
             log(f"On search request here is {request_payload['context']}")
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_SEARCH)
+            return final_resp
         else:
             return resp
 
@@ -34,10 +36,12 @@ class OnSelectRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_select", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_select", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_SELECT)
+            return final_resp
         else:
             return resp
 
@@ -48,10 +52,12 @@ class OnInitRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_init", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_init", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_INIT)
+            return final_resp
         else:
             return resp
 
@@ -63,10 +69,12 @@ class OnConfirmRequest(Resource):
     def post(self):
         request_payload = request.get_json()
         log(f"request payload for on_confirm {request_payload}")
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_confirm", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_confirm", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_CONFIRM)
+            return final_resp
         else:
             return resp
 
@@ -77,10 +85,12 @@ class OnCancelRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_cancel", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_cancel", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_CANCEL)
+            return final_resp
         else:
             return resp
 
@@ -91,10 +101,12 @@ class OnCancellationReasonsRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_cancellation_reasons", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_cancellation_reasons", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_CANCELLATION_REASONS)
+            return final_resp
         else:
             return resp
 
@@ -105,8 +117,7 @@ class OnIssueRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_issue", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_issue", domain="logistics")
         if resp is None:
             dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
                                  action=request_payload[constant.CONTEXT]["action"])
@@ -122,8 +133,7 @@ class OnIssueStatusRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_issue_status", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_issue_status", domain="logistics")
         if resp is None:
             dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
                                  action=request_payload[constant.CONTEXT]["action"])
@@ -139,10 +149,12 @@ class OnRatingRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_rating", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_rating", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_RATING)
+            return final_resp
         else:
             return resp
 
@@ -153,10 +165,12 @@ class OnStatusRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_status", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_status", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_STATUS)
+            return final_resp
         else:
             return resp
 
@@ -167,10 +181,12 @@ class OnSupportRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_support", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_support", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_SUPPORT)
+            return final_resp
         else:
             return resp
 
@@ -181,10 +197,12 @@ class OnTrackRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_track", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_track", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_TRACK)
+            return final_resp
         else:
             return resp
 
@@ -195,9 +213,11 @@ class OnUpdateRequest(Resource):
     @validate_auth_header
     def post(self):
         request_payload = request.get_json()
-        resp = validate_payload_schema_based_on_version(
-            request_payload, "on_update", domain="logistics")
+        resp = validate_payload_schema_based_on_version(request_payload, "on_update", domain="logistics")
         if resp is None:
-            return dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value, action=request_payload[constant.CONTEXT]["action"])
+            final_resp = dump_request_payload(request_payload, domain=OndcDomain.LOGISTICS.value,
+                                              action=request_payload[constant.CONTEXT]["action"])
+            send_logistics_payload_to_client(request_payload, OndcAction.ON_UPDATE)
+            return final_resp
         else:
             return resp
